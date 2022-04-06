6040bc175a4c2e802b4b8fdae544d921b91164d9
sgarc <gallegogarciasergio@gmail.com>
2022-04-06 19:41:32 +0100

    Day 7 - Recap and Adding other Files

---
+++ Chapter2/IndentingCode/app.js
@@ -0,0 +1,3 @@
+// ! rule of thumb
+
+// indent code following the best ease of readability and uniformity within the group/team you work in for easier collaboration
---
+++ Chapter2/forLoops/app.js
@@ -0,0 +1,15 @@
+let number = 0;
+
+// ! earlier we saw this code which prints even numbers until 12
+
+while (number <= 12) {
+  console.log(number);
+  number = number + 2;
+}
+
+// output -> 0 -> 2 -> 4... -> 12
+
+/* 
+  * the for loop groups together all the statements related to the "state" of the loop
+  ? 
+*/
